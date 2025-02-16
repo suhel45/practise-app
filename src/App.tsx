6e33c41633297bedae08da5938633447b70503de
suhel45 <suhel.ict45@gmail.com>
2025-02-16 23:13:41 +0600

    all file added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import FormSubmission from "./component/FormSubmission"
+
+function App() { 
+  return(
+    <>
+    <h1>hello</h1>
+    <FormSubmission/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/FormSubmission.tsx
@@ -0,0 +1,43 @@
+import { useActionState } from "react"
+
+
+interface messageData {
+    message?: string,
+    error?:string,
+}
+const handleSubmit  = async(state: string | undefined | object , formData: FormData):Promise<string | undefined | object>=>{
+    const name = formData.get("name");
+    const email = formData.get("email");
+    await new Promise(res=>setTimeout(res,2000));
+    console.log("submit" ,name, email)
+    if(name && email){
+        return {message:"Form submitted successfully"};
+    }
+    else{
+        return {error:"Form submission failed"};
+    }
+    }
+const FormSubmission = () => {
+    const [data,action, ispending] = useActionState(handleSubmit, undefined);
+    console.log("data",data)
+  return (
+    <>
+    <div>FormSubmission</div>
+    <form action={action}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+        <br />
+        <label htmlFor="email">Email:</label>
+        <input type="text" id="name" name="email"  />
+        <br />
+        <button type="submit" disabled={ispending}>
+           {ispending?"submitting...":"submit"}
+            </button>
+    </form>
+    {<span style={{color:"green"}}>{(data as messageData)?.message}</span>}
+    <span style={{color:"red"}}>{(data as messageData)?.error}</span>
+    </>
+  )
+}
+
+export default FormSubmission
