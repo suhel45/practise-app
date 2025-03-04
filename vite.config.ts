632ee402932037d44c6641c780521d61e1a19e30
suhel45 <suhel.ict45@gmail.com>
2025-03-04 12:44:54 +0600

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/practise-app/",
+  base: "/practise-app/", 
+  build: {
+    outDir: "dist",  
+    rollupOptions: {
+      input: "index.html", 
+    },
+  },
 });
